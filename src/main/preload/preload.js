964da138963ed0b725d909e701a7137b16723150
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-12-12 14:34:30 -0500

    changed interop.setContext from being remote dependant to sending IPC invocations

--- src/main/preload/preload.js
+++ src/main/preload/preload.js
@@ -96,46 +96,16 @@ interop.dialog = Object.freeze({
 	}
 })
 
-
-const textElements = 'input[type="text"], textarea'
-
 interop.setContextMenu = () => {
-	const menu = new remote.Menu()
-
-	if (process.env.NODE_ENV === 'development') {
-		let pos = {}
-		
-		menu.append(new remote.MenuItem({
-			label: 'Inspect Element',
-			click() {
-				remote.getCurrentWindow().inspectElement(pos.x, pos.y)
-			}
-		}))
+	const textElement = 'input[type="text"], input[type="number"]'
 	
-		window.addEventListener('contextmenu', e => {
-			e.preventDefault()
-			pos = { x: e.x, y: e.y }
-			menu.popup(remote.getCurrentWindow())
+	window.addEventListener('contextmenu', e => {
+		ipcRenderer.invoke('getContextMenu', {
+			isTextElement: e.target.matches(textElement) && !e.target.disabled,
+			x: e.x,
+			y: e.y
 		})
-	} else {
-		const menuItems = [
-			new remote.MenuItem({ role: 'cut' }),
-			new remote.MenuItem({ role: 'copy' }),
-			new remote.MenuItem({ role: 'paste' }),
-			new remote.MenuItem({ type: 'separator' }),
-			new remote.MenuItem({ role: 'selectAll' })
-		]
-	
-		menuItems.forEach(item => menu.append(item))
-	
-		window.addEventListener('contextmenu', e => {
-			e.preventDefault()
-	
-			if (e.target.matches(textElements) && !e.target.disabled) {
-				menu.popup(remote.getCurrentWindow())
-			}
-		})
-	}
+	})
 }
 
 // ---- ATTACH ALL TO RENDERER--------
