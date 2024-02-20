6907582f712d528a362575fd9aa16a00108ae484
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2024-02-20 02:48:34 -0500

    changed canvas drawings to new source look

--- src/renderer/actions/main/buildSource.js
+++ src/renderer/actions/main/buildSource.js
@@ -1,44 +1,55 @@
 const layout = {
-	'720': {
-		fontSize: 25,
-		txtX: 640, 
-		txtY: 664,
-		minW: 330,
-		pad: 40,
-		boxY: 639,
-		boxH: 33
-	},
-	'1080': {
-		fontSize: 37.5,
-		txtX: 960,
-		txtY: 996,
-		minW: 495,
-		pad: 60,
-		boxY: 958.5,
-		boxH: 49.5
-	}
+	'720': onTop => ({
+		fontSize: 13.777778,
+		txtX: onTop ? 38 : 1242, 
+		txtY: onTop ? 171.333333 : 563.333333,
+		maxW: 322,
+		pad: 18,
+		boxY: onTop ? 148 : 539.333333,
+		boxH: 33.333333
+	}),
+	'1080': onTop => ({
+		fontSize: 20.666667,
+		txtX: onTop ? 57 : 1863,
+		txtY: onTop ? 257 : 845,
+		maxW: 483,
+		pad: 28,
+		boxY: onTop ? 222 : 809,
+		boxH: 50
+	})
 }
 
-export default (src, output) => {
+export default (src, output, sourceOnTop) => {
 	const cnv = document.createElement('canvas')
 	const ctx = cnv.getContext('2d')
 	const [ width, height ] = output.split('x')
-	const { fontSize, txtX, txtY, minW, pad, boxY, boxH } = layout[height]
+	const { fontSize, txtX, txtY, maxW, pad, boxY, boxH } = layout[height](sourceOnTop)
 
 	cnv.width = width
 	cnv.height = height
 
 	ctx.globalCompositeOperation = 'destination-over'
-
 	ctx.fillStyle = '#ffffff'
-	ctx.font      = `${fontSize}px Gotham`
-	ctx.textAlign = 'center'
-	ctx.fillText(src, txtX, txtY)
 
-	const txtW = Math.max(minW, ctx.measureText(src).width + pad)
+	ctx.font = `${fontSize}px Inter`
+	ctx.save()
+	ctx.globalCompositeOperation = 'source-over'
+	ctx.textAlign = sourceOnTop ? 'left' : 'right'
+	ctx.textBaseline = 'bottom'
+	ctx.lineWidth = 2
+	ctx.strokeStyle = '#000000'
+	ctx.shadowColor = '#000000'
+	ctx.shadowOffsetX = Math.cos(5.35816) + 1 // Converted from .psd that has the shadow angle at 127° and distance at 1px
+	ctx.shadowOffsetY = Math.sin(5.35816) + 1
+	ctx.strokeText(src, txtX, txtY, maxW)
+	ctx.fillText(src, txtX, txtY, maxW)
+	ctx.restore()
+
+	const txtW = Math.min(ctx.measureText(src).width, maxW)
+	const boxW = (sourceOnTop ? txtX : width - txtX) + txtW + pad
 
 	ctx.fillStyle = 'rgba(0,0,0,0.4)'
-	ctx.fillRect(txtX - txtW / 2, boxY, txtW, boxH)
+	ctx.fillRect(sourceOnTop ? 0 : width - boxW, boxY, boxW, boxH)
 
 	return cnv.toDataURL().replace(/^data:image\/\w+;base64,/, '')
 }
