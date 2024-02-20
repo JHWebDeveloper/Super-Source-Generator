8f67776a0e5e73cb9baf12969837acd50faeaab8
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2024-02-20 02:46:39 -0500

    added hooks to preload font Inter-SemiBold before app is accessible

--- src/renderer/components/main/App.js
+++ src/renderer/components/main/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../css/index.css'
 
 import { PrefsProvider } from '../../store/prefsStore'
@@ -6,14 +6,36 @@ import Header from './Header'
 import Update from '../update/Update'
 import Main from './Main'
 
-const App = () => (
-	<>
-		<Header />
-		<Update />
-		<PrefsProvider>
-			<Main />
-		</PrefsProvider>
-	</>
-)
+const fonts = [
+	['Inter', 'url(./assets/font/Inter-SemiBold.woff2)']
+]
+
+const App = () => {
+	const [ fontsLoaded, setFontsLoaded ] = useState(false)
+
+	useEffect(() => {
+    (async () => {
+      await Promise.all(fonts.map(async fontAttr => {
+        const font = new FontFace(...fontAttr)
+
+        await font.load()
+
+        document.fonts.add(font)
+      }))
+
+      setFontsLoaded(true)
+    })()
+  }, [])
+
+	return fontsLoaded ? (
+		<>
+			<Header />
+			<Update />
+			<PrefsProvider>
+				<Main />
+			</PrefsProvider>
+		</>
+	) : <></>
+}
 
 export default App
